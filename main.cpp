d954f7ba6d46d4556b3519d14023b4312361a0eb
yeontachi <yeonjjhh@gmail.com>
2025-10-18 00:01:57 +0900

    Noise Filtering

--- main.cpp
+++ main.cpp
@@ -4,60 +4,122 @@
 using namespace std;
 using namespace cv;
 
-static Mat src, grayImg, edges, vis;
-static int cannyLow = 50, cannyHigh = 150;
-static int rhoTimes10 = 10;
-static int thetaDeg = 1;
-static int houghThresh = 50;
-static int minLineLen = 50;
-static int maxLineGap = 10;
-
-static void run(int = 0, void * = nullptr)
+static inline unsigned char clamp8(int v)
 {
-    Canny(grayImg, edges, cannyLow, cannyHigh);
+    return (unsigned char)std::max(0, std::min(255, v));
+}
 
-    // 허프 라인 디텍터
-    double rho = max(1, rhoTimes10) / 10.0; // 0방지
-    double theta = thetaDeg * CV_PI / 180.0;
-    vector<Vec4i> lines;
-    HoughLinesP(edges, lines, rho, theta, houghThresh, (double)minLineLen, (double)maxLineGap);
+// Zero Padding
+Mat ZeroPadding(const Mat &src, int pad)
+{
+    Mat paddedMat = Mat::zeros(src.rows + 2 * pad, src.cols + 2 * pad, CV_8UC1);
 
-    cvtColor(edges, vis, COLOR_GRAY2BGR);
-    for (const auto &l : lines)
-        line(vis, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 255, 0), 2, LINE_AA);
+    for (int h = 0; h < src.rows; ++h)
+    {
+        for (int w = 0; w < src.cols; ++w)
+        {
+            paddedMat.at<uchar>(h + pad, w + pad) = src.at<uchar>(h, w);
+        }
+    }
 
-    putText(vis, format("Canny=(%d,%d)  rho=%.1f  theta=%ddeg  thr=%d  minLen=%d  maxGap=%d  #lines=%zu", cannyLow, cannyHigh, rho, thetaDeg, houghThresh, minLineLen, maxLineGap, lines.size()),
-            Point(10, 25), FONT_HERSHEY_SIMPLEX, 0.6, Scalar(0, 200, 255), 2);
+    return paddedMat;
+}
 
-    imshow("edges", edges);
-    imshow("HoughLinesP result", vis);
+// Median Filter
+Mat MedianFilter(const Mat &src, int ksize)
+{
+    if (src.empty())
+        throw runtime_error("empty Image");
+    if (src.type() != CV_8UC1)
+        throw runtime_error("use 8-bit grayscale");
+    if (ksize <= 0 || (ksize % 2) == 0)
+        throw runtime_error("ksize must be odd and >0");
+
+    const int pad = ksize / 2;
+    Mat padded = ZeroPadding(src, pad);
+    Mat dst(src.rows, src.cols, CV_8UC1, Scalar(0));
+
+    const int W = padded.cols;
+    const unsigned char *pData = padded.ptr<unsigned char>(0);
+    unsigned char *outData = dst.ptr<unsigned char>(0);
+
+    for (int h = pad; h < padded.rows - pad; ++h)
+    {
+        for (int w = pad; w < padded.cols - pad; ++w)
+        {
+            vector<uchar> window;
+            window.reserve(ksize * ksize);
+
+            for (int y = -pad; y <= pad; ++y)
+            {
+                const uchar *row = padded.ptr<uchar>(h + y);
+                for (int x = -pad; x <= pad; ++x)
+                {
+                    window.push_back(row[w + x]);
+                }
+            }
+
+            // 오름차순 정렬
+            sort(window.begin(), window.end());
+
+            // median
+            int mid = window.size() / 2;
+            outData[(h - pad) * src.cols + (w - pad)] = window[mid];
+        }
+    }
+    return dst;
 }
 
 int main(void)
 {
-    Mat src = imread("highway.png");
-
-    if (src.empty())
+    Mat GN10 = imread("GaussianNoise10.png", IMREAD_GRAYSCALE); // 가우시안 노이즈 이미지
+    if (GN10.empty())
+    {
+        cerr << "Image Not Found" << "\n";
+        return -1;
+    }
+    Mat GN25 = imread("GaussianNoise25.png", IMREAD_GRAYSCALE); // 가우시안 노이즈 이미지
+    if (GN25.empty())
     {
         cerr << "Image Not Found" << "\n";
         return -1;
     }
 
-    cvtColor(src, grayImg, COLOR_BGR2GRAY);
-    GaussianBlur(grayImg, grayImg, Size(3, 3), 0.8);
+    Mat SPN005 = imread("lena512SPN005.png", IMREAD_GRAYSCALE); // 소금후추 노이즈 이미지
+    if (SPN005.empty())
+    {
+        cerr << "Image Not Found" << "\n";
+        return -1;
+    }
+    Mat SPN010 = imread("lena512SPN010.png", IMREAD_GRAYSCALE); // 소금후추 노이즈 이미지
+    if (SPN010.empty())
+    {
+        cerr << "Image Not Found" << "\n";
+        return -1;
+    }
 
-    namedWindow("HoughLinesP result", WINDOW_AUTOSIZE);
-    namedWindow("edges", WINDOW_AUTOSIZE);
+    int ksize = 3; // filter 사이즈
+    cout << "커널 사이즈: ";
+    cin >> ksize;
 
-    createTrackbar("CannyLow", "HoughLinesP result", &cannyLow, 300, run);
-    createTrackbar("CannyHigh", "HoughLinesP result", &cannyHigh, 400, run);
-    createTrackbar("rho x0.1", "HoughLinesP result", &rhoTimes10, 50, run); // 0.1~5.0
-    createTrackbar("theta deg", "HoughLinesP result", &thetaDeg, 5, run);   // 1~5 deg
-    createTrackbar("threshold", "HoughLinesP result", &houghThresh, 300, run);
-    createTrackbar("minLineLen", "HoughLinesP result", &minLineLen, 500, run);
-    createTrackbar("maxLineGap", "HoughLinesP result", &maxLineGap, 200, run);
+    Mat GN10out = MedianFilter(GN10, ksize);
+    Mat GN25out = MedianFilter(GN25, ksize);
 
-    run();
+    Mat SPN005out = MedianFilter(SPN005, ksize);
+    Mat SPN010out = MedianFilter(SPN010, ksize);
+
+    imshow("Gaussian Noise10", GN10);
+    imshow("Gaussian Noise25", GN25);
+    imshow("Salt and Pepper Noise 0.05", SPN005);
+    imshow("Salt and Pepper Noise 0.10", SPN010);
+
+    imshow("GN10_MedianFilter", GN10out);
+    imshow("GN25_MedianFilter", GN25out);
+
+    imshow("SPN005_MedianFilter", SPN005out);
+    imshow("SPN010_MedianFilter", SPN010out);
+
     waitKey(0);
+
     return 0;
 }
